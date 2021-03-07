848c539f8205f5198f4bf12d0322698f8a71fb50
kubala2908 <j.b.tuminski@gmail.com>
2021-03-07 17:47:15 +0100

    plan_details

--- src/main/java/pl/coderslab/dao/DayNameDao.java
+++ src/main/java/pl/coderslab/dao/DayNameDao.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class DayNameDao {
     private static final String FIND_ALL_DAY_NAMES_QUERY = "SELECT * FROM day_name ORDER BY display_order ASC;";
-
+    private static final String FIND_DAYS_BY_PLAN_ID_QUERY ="SELECT * FROM day_name LEFT JOIN recipe_plan rp on day_name.id = rp.day_name_id WHERE plan_id=? ORDER BY day_name.display_order ASC";
     // tworzy listę obiektów DayName z bazy danych, wedłóg kolejności display_order
     public List<DayName> findAll() {
         List<DayName> DayNameList = new ArrayList<>();
@@ -21,6 +21,27 @@ public class DayNameDao {
              PreparedStatement statement = connection.prepareStatement(FIND_ALL_DAY_NAMES_QUERY);
              ResultSet resultSet = statement.executeQuery()) {
 
+            while (resultSet.next()) {
+                DayName DayNameToAdd = new DayName();
+                DayNameToAdd.setId(resultSet.getInt("id"));
+                DayNameToAdd.setName(resultSet.getString("name"));
+                DayNameToAdd.setDisplayOrder(resultSet.getInt("display_order"));
+                DayNameList.add(DayNameToAdd);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return DayNameList;
+    }
+
+    public List<DayName> findDays(int planId) {
+        List<DayName> DayNameList = new ArrayList<>();
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_DAYS_BY_PLAN_ID_QUERY);) {
+
+            statement.setInt(1, planId);
+            ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 DayName DayNameToAdd = new DayName();
                 DayNameToAdd.setId(resultSet.getInt("id"));
--- src/main/java/pl/coderslab/dao/RecipePlanDao.java
+++ src/main/java/pl/coderslab/dao/RecipePlanDao.java
@@ -1,11 +1,18 @@
 package pl.coderslab.dao;
 
+import pl.coderslab.model.DayName;
+import pl.coderslab.model.RecipePlan;
 import pl.coderslab.utils.DbUtil;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipePlanDao {
     private static final String CREATE_RECIPE_PLAN_QUERY = "INSERT INTO recipe_plan(recipe_id, meal_name, display_order, day_name_id, plan_id) VALUES (?,?,?,?,?);";
+    private static final String FIND_BY_ID_QUERY = "SELECT * FROM recipe LEFT JOIN recipe_plan rp on recipe.id = rp.recipe_id WHERE plan_id=? ORDER BY display_order ASC;";
 
     public void create(int planId, String mealName, int displayOrder, int recipeiD, int dayNameId) {
         try (Connection connection = DbUtil.getConnection();
@@ -24,4 +31,31 @@ public class RecipePlanDao {
             e.printStackTrace();
         }
     }
+
+    public List<RecipePlan> findById(int planId) {
+        List<RecipePlan> DayNameList = new ArrayList<>();
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_QUERY);) {
+
+            statement.setInt(1, planId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                RecipePlan RecipePlanAdd = new RecipePlan();
+                RecipePlanAdd.setId(resultSet.getInt("id"));
+                RecipePlanAdd.setMealName(resultSet.getString("meal_name"));
+                RecipePlanAdd.setDisplay_order(resultSet.getInt("display_order"));
+                RecipePlanAdd.setDay_name_id(resultSet.getInt("day_name_id"));
+                RecipePlanAdd.setPlanId(resultSet.getInt("plan_id"));
+                RecipePlanAdd.setRecipeId(resultSet.getInt("recipe_id"));
+                RecipePlanAdd.setRecipeDescri(resultSet.getString("description"));
+                DayNameList.add(RecipePlanAdd);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return DayNameList;
+    }
+
+
 }
